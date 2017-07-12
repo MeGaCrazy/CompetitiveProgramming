514fdfae784b2e61aad421e7049de902e466953e
MichaelMounir12 <mikaelmounir@gmail.com>
2017-07-12 10:25:28 +0200

    solve CF151_D2_C

---
+++ Codeforces/CF151-D2-C.cpp
@@ -0,0 +1,32 @@
+/*
+*   target : whose win and if 1 count the number of moves
+*     sol  : from observation that player 2 win only if
+*            the number (q) is prime number and the num of prime factors not greater than or equal to 2 :) 
+*
+*
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+       long long n,ans=1;
+       scanf("%lld",&n);
+       int cnt=0;
+       for(long long i=2;i*i<=n;i++){
+           while(n%i==0&&i*i<=n){
+              cnt++;
+              n/=i;
+              ans*=i;
+              if(cnt==2)break;
+           }
+           if(cnt==2)break;
+       }
+       if(cnt>=2){printf("1\n%lld\n",ans);}
+       else if(cnt==1){
+         printf("2");
+       }else{
+          printf("1\n0");
+       }
+
+}
