8fa8afcc3ed017540ec2764eeabb5d0e82a2856e
MichaelMounir12 <mikaelmounir@gmail.com>
2017-07-13 05:43:55 +0200

    Update LiveArchive_3634.cpp

--- LiveArchive/LiveArchive_3634.cpp
+++ LiveArchive/LiveArchive_3634.cpp
@@ -3,7 +3,7 @@ using namespace std;
 map<set<int>,int>mpst;
 map<int,set<int> >mpts;
 
-int get_size(set<int>s){
+int get_index(set<int>s){
    
    int &a=mpst[s];
    if(a==0){
@@ -25,7 +25,7 @@ int main(){
            while(n--){
               scanf("%s",str);
               if(!strcmp(str,"PUSH")){
-                 s.push(get_size(set<int>()));
+                 s.push(get_index(set<int>()));
               }else if(!strcmp(str,"DUP")){
                  s.push(s.top());
               }else if(!strcmp(str,"UNION")){
@@ -33,19 +33,19 @@ int main(){
                  int b=s.top();s.pop();
                  set<int> &aa=mpts[a],&bb=mpts[b],c;
                  set_union(aa.begin(),aa.end(),bb.begin(),bb.end(),inserter(c,c.begin()));
-                 s.push(get_size(c));
+                 s.push(get_index(c));
               }else if(!strcmp(str,"INTERSECT")){
                  int a=s.top();s.pop();
                  int b=s.top();s.pop();
                  set<int> &aa=mpts[a],&bb=mpts[b],c;
                  set_intersection(aa.begin(),aa.end(),bb.begin(),bb.end(),inserter(c,c.begin()));
-                 s.push(get_size(c));
+                 s.push(get_index(c));
               }else if(!strcmp(str,"ADD")){
                 int a=s.top();s.pop();
                 int b=s.top();s.pop();
                 set<int>bb=mpts[b];
                 bb.insert(a);
-                s.push(get_size(bb));
+                s.push(get_index(bb));
               }                  
              printf("%d\n",(int)mpts[(int)s.top()].size());
            }
