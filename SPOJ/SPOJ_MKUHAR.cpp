652093dba6762c1b91a2e2d60061d83b6f78bf13
MichaelMounir12 <mikaelmounir@gmail.com>
2017-07-30 21:57:18 +0200

    Add header for SPOJ_MKUHAR.cpp

--- SPOJ/SPOJ_MKUHAR.cpp
+++ SPOJ/SPOJ_MKUHAR.cpp
@@ -3,60 +3,61 @@
 *   and simulate the process 
 *
 */
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 #define oo 100000000
- 
-int ing[101][6];
+
+int arr[101][6];
 int n, budget;
 bool chk;
 bool check(int srg)
 {
-    int minBudget = 0;
-    for(int i = 0; i < n; ++i)
-    {
-        int diff = ing[i][1] - ing[i][0] * srg;
-        if(diff < 0)
-        {
-            diff *= -1;
-            int best = oo;
-            for(int j = 0; ; ++j)
-            {
-                int t;
-                if(j * ing[i][2] >= diff)
-                    t = j * ing[i][3];
-                else
-                    t = j * ing[i][3] + (int)ceil( (diff - j * ing[i][2]) / (1.0*ing[i][4]) ) * ing[i][5];
-                best =  min(best, t);
-                if(j * ing[i][2] >= diff)
-                    break;
-            }
-            minBudget += best;
-        }
-    }
-    return minBudget <= budget;
+	int minBudget = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		int diff = arr[i][1] - arr[i][0] * srg;
+		if (diff < 0)
+		{
+			diff *= -1;
+			int best = oo;
+			for (int j = 0; ; ++j)
+			{
+				int t;
+				if (j * arr[i][2] >= diff)
+					t = j * arr[i][3];
+				else
+					t = j * arr[i][3] + (int)ceil((diff - j * arr[i][2]) / (1.0*arr[i][4])) * arr[i][5];
+				best = min(best, t);
+				if (j * arr[i][2] >= diff)
+					break;
+			}
+			minBudget += best;
+		}
+	}
+	return minBudget <= budget;
 }
- 
- 
+
+
 int main()
 {
-//    freopen("in.txt", "r", stdin);
-    cin >> n >> budget;
-    for(int i = 0; i < n; ++i)
-        cin >> ing[i][0] >> ing[i][1] >> ing[i][2] >> ing[i][3] >> ing[i][4] >> ing[i][5];
-    int l = 0, u = budget/n + 100, ans;
-    while(l <= u)
-    {
-        int mid = (l+u)/2;
-        if(check(mid))
-        {
-            ans = mid;
-            l = mid+1;
-        }
-        else
-            u = mid-1;
-    }
-    cout << ans << endl;
-    return 0;
+#ifndef ONLINE_JUDGE
+       freopen("in.txt","r",stdin);
+#endif
+	cin >> n >> budget;
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i][0] >> arr[i][1] >> arr[i][2] >> arr[i][3] >> arr[i][4] >> arr[i][5];
+	int l = 0, u = budget / n + 100, ans;
+	while (l <= u)
+	{
+		int mid = (l + u) / 2;
+		if (check(mid))
+		{
+			ans = mid;
+			l = mid + 1;
+		}
+		else
+			u = mid - 1;
+	}
+	printf("%d", ans);
+	return 0;
 }
