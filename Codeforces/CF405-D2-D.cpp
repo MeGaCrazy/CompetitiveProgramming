995a15e1405da9fb0d84b1343b9ad63798fb3b19
MichaelMounir12 <mikaelmounir@gmail.com>
2017-08-16 02:49:51 +0200

    Update CF405-D2-D.cpp

--- Codeforces/CF405-D2-D.cpp
+++ Codeforces/CF405-D2-D.cpp
@@ -1,5 +1,16 @@
 /*
-*         First Step really easy Just get the symmetric of the i or ((int)1e+6 - i +1) where i (1<=i<=(int)1e+6)
+*         The Idea :
+*                  Always make s(SizeOfSet) = (int)1e6 then 
+*                  1-First Step
+*                  to get element y take the symmetric of element of x But make sure that not exit in x
+*                  The symmetric of i  = ((s)-i+1) :) and vice versa 
+*                  2- Second Step
+*                      if i and his symmetric already in X Just take any (j) and his symmetric that don't belong to x
+*                      For illustration take this sample :)
+*                          if X ={1,1000000} Then the Sum of Formula of X is =(1000000-1)+(1000000-1000000)=999999
+*                          take any i and his symmetric don't belong to x like
+*                          i=2 his symmetric = 999999 Then Sum of Forumla of Y is=(1000000-2)+(1000000-999999)=999999
+*                          Happy Solving :)    
 *
 *
 */
