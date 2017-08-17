e45a9b4884fc9cc49dee317b49fae14d40d0bd7e
MichaelMounir12 <mikaelmounir@gmail.com>
2017-08-18 01:01:24 +0200

    Update SPOJ_GSS3

--- SPOJ/SPOJ_GSS3.cpp
+++ SPOJ/SPOJ_GSS3.cpp
@@ -14,12 +14,12 @@ node join(node left, node right) {
 	return node({ 
 		max(max(left.maxsum,right.maxsum),left.rightsum + right.leftsum),
 		left.bothsum + right.bothsum,
-		max(left.maxsum,left.bothsum + right.leftsum)
-		,max(right.maxsum,right.bothsum + left.rightsum) });
+		max(left.leftsum,left.bothsum + right.leftsum)
+		,max(right.rightsum,right.bothsum + left.rightsum) });
 }
 void build(int p, int l, int r) {
 	if (l == r) {
-		st[p].maxsum = st[p].bothsum = st[p].leftsum = st[p].rightsum = arr[l];
+		st[p]=node({arr[l],arr[l],arr[l],arr[l]});
 		return;
 	}
 	else {
@@ -31,36 +31,33 @@ void build(int p, int l, int r) {
 	}
 }
 void update(int p, int l, int r, int x, int value) {
-	if (x>r || l>x)return;
 	if (l == r&&x == r) {
 		st[p] = { value,value,value,value };
 		return;
-	}
+         }
 	int mid = (l + r) >> 1;
 	int np = p << 1;
 	if (x <= mid)
-		update(np, l, mid, x, value);
+	update(np, l, mid, x, value);
+	else
 	update(np + 1, mid + 1, r, x, value);
-	st[p] = join(st[np], st[np + 1]);
+
+        st[p]=join(st[np],st[np+1]);
 }
 node query(int p, int l, int r, int x, int y) {
 	if (l>y || x>r) {
-		return node({ (int)-1e9,(int)-1e9,(int)-1e9,(int)-1e9 });
+		return node({ -999999,-999999,-999999,-999999 });
 	}
 	if (l >= x&&r <= y)return st[p];
 	int mid = (l + r) >> 1;
 	int np = p << 1;
 	node p1 = query(np, l, mid, x, y);
 	node p2 = query(np + 1, mid + 1, r, x, y);
-	if (p1.bothsum == (int)-1e9)
-		return p2;
-	if (p2.bothsum == (int)-1e9)
-		return p1;
 	return join(p1, p2);
 }
 int main() {
 #ifndef ONLINE_JUDGE
-	//freopen("in.txt", "r", stdin);
+	freopen("in", "r", stdin);
 #endif
 	int n;
 	scanf("%d", &n);
