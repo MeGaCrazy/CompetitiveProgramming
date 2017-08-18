0d869a5fb21c2697e65cb0e09036d451f1ac5f1d
MichaelMounir12 <mikaelmounir@gmail.com>
2017-08-18 02:05:48 +0200

    Solve SPOJ_QUEEN

--- SPOJ/SPOJ_QUEEN.cpp
+++ SPOJ/SPOJ_QUEEN.cpp
@@ -1,76 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int N = 1005;
-int x[] = { 0,0,-1,1,1,-1,-1,1 };
-int y[] = { -1,1,0,0,1,-1,1,-1 };
-int dis[N][N][10];
+const int N=1005;
+int x[8]={1,-1,0,0,1,-1,1,-1};
+int y[8]={0,0,-1,1,1,-1,-1,1};
+int dis[N][N];
 char arr[N][N];
-int r, c;
-int ansr, ansc;
-bool check(int xx, int yy) {
-	return xx >= 1 && yy >= 1 && xx <= r&&yy <= c && (arr[xx][yy] == '.' || arr[xx][yy] == 'F');
+bool vis[N][N][8];
+int r,c;
+bool check(int xx,int yy){
+   return xx>=0&&yy>=0&&xx<r&&yy<c&&(arr[xx][yy]=='.'||arr[xx][yy]=='F');
 }
-int bfs(int st, int en) {
-	/*
-	for (int i = 1; i <= r; i++) {
-	for (int j = 1; j <= c; j++) {
-	for(int k=0;k<10;k++){
-	dis[i][j][k] = (int)2e9;
-	}
-	}
-	}                  */
-	memset(dis, -1, sizeof(dis));
-	queue<pair<int, pair<int, int> > >q;
-	q.push(make_pair(9, make_pair(st, en)));
-	dis[st][en][9] = 0;
-	while (!q.empty()) {
-		pair<int, pair<int, int> >u = q.front();
-		q.pop();
-		if (u.second.first == ansr&&u.second.second == ansc)return dis[u.second.first][u.second.second][u.first];
-		//return dis[u.second.first][u.second.second][u.first];
-		if (arr[u.second.first][u.second.second] == 'X')continue;
-		for (int i = 0; i<8; i++) {
-			int xx = u.second.first + x[i], yy = u.second.second + y[i];
-			if (check(xx, yy)) {
-				int cost = (u.first == i) ? dis[u.second.first][u.second.second][u.first] : dis[u.second.first][u.second.second][u.first] + 1;
-				if (dis[xx][yy][i] == -1 || dis[xx][yy][i]>(cost)) {
-					dis[xx][yy][i] = (cost);
-					q.push(make_pair(i, make_pair(xx, yy)));
-				}
-			}
-		}
-	}
-		return -1;
+int st,en;
+int ansr,ansc;
+int bfs(){
+      for(int i=0;i<=r;i++){
+         for(int j=0;j<=c;j++){
+            dis[i][j]=(int)2e9;
+         }
+      }
+      memset(vis,0,sizeof(vis));
+      for(int i=0;i<8;i++){
+       vis[st][en][i]=1;
+      }
+      queue<pair<int,int> >q;
+      q.push(make_pair(st,en));
+      dis[st][en]=0;
+      while(!q.empty()){
+          pair<int,int>u=q.front(); 
+          if(u.first==ansr&&u.second==ansc)return dis[u.first][u.second];
+          q.pop();
+          for(int i=0;i<8;i++){
+             int xx=u.first+x[i],yy=u.second+y[i];
+             while(check(xx,yy)==1){
+                if(dis[xx][yy]>(dis[u.first][u.second]+1)){
+                   dis[xx][yy]=(dis[u.first][u.second]+1);
+                   q.push(make_pair(xx,yy));
+                }else{
+                 if(vis[xx][yy][i])break;
+                }
+                vis[xx][yy][i]=1;
+                xx+=x[i],yy+=y[i];
+             }
+          }
+      }
+      return -1;
 }
-int main() {
+int main(){
 #ifndef ONLINE_JUDGE
-	freopen("in", "r", stdin);
+      //  freopen("in","r",stdin);
 #endif
-	int t;
-	scanf("%d", &t);
-	while (t--) {
-		scanf("%d %d", &r, &c);
-		int st, en;
-		for (int i = 1; i <= r; i++) {
-			for (int j = 1; j <= c; j++) {
-				scanf(" %c", arr[i] + j);
-				if (arr[i][j] == 'F')ansr = i, ansc = j;
-				if (arr[i][j] == 'S')st = i, en = j;
-			}
-		}
-			printf("%d\n",bfs(st,en));
-		/*
-		int ans = (int)2e9;
-		for (int i = 0; i<8; i++) {
-			if (dis[ansr][ansc][i] == -1)continue;
-			ans = min(ans, dis[ansr][ansc][i]);
-			cerr << dis[ansr][ansc][i] << endl;
-		}
-
-
-		printf("%d\n", ans);
-		*/
-
-		fprintf(stderr, "The Run Time is %d ms \n", (int)clock());
-	}
+       int t;
+       scanf("%d",&t);
+       while(t--){
+          scanf("%d %d",&r,&c);
+          for(int i=0;i<r;i++){
+              cin>>arr[i];              
+          }
+          for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(arr[i][j]=='F'){
+                   ansr=i,ansc=j;
+                   continue;
+               }
+               if(arr[i][j]=='S'){
+                 st=i,en=j;
+                 continue;
+               }
+            }
+          }
+          printf("%d\n",bfs());
+      }
 }
