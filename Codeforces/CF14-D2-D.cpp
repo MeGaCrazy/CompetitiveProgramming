19e07e1dd546f5d0c67c2e7a60f08d3f579061d7
MichaelMounir12 <mikaelmounir@gmail.com>
2017-08-22 00:30:42 +0200

    Solve CF14-D2-D

--- Codeforces/CF14-D2-D.cpp
+++ Codeforces/CF14-D2-D.cpp
@@ -1,40 +1,55 @@
+/*
+*     Idea : The solution of that problem is the max diameter of Tree of  First path *  max Diameter of Second;
+*            when it not Be  intersect if u have edges like that for example :
+*                                     1-2 2-3 3-4 
+*                                     so if u can take from left node to other node that not interset 
+*                                     in other word if i don't use my edge and make my edge like let we say 
+*                                     The break of The two tree make that for every edge to get Max diameter for left and right then multiply              
+*                                     Happy Solving :)
+*/                                                 
+
 #include <bits/stdc++.h>
 using namespace std;
 const int N=205;
-bool adj[N][N];
-int dep[N];
-int ans,n;
-int dfs(int i,int pre){
-     for(int j=1;j<=n;j++){
-         if(adj[i][j]&&j!=pre){
-              dfs(j,i);
-              ans=max(ans,dep[j]+1+dep[i]);
-              dep[i]=max(dep[i],dep[j]+1);
-         }
-     }
-    return ans;
+vector<int>adj[N];
+vector<pair<int,int> >edges;
+pair<int,int>cur_edge;
+int ans;
+int mxdp;
+int v;
+void dfs(int u,int p,int depth){
+       if(depth>mxdp){
+          mxdp=depth;
+          v=u;
+       }
+       for(int i=0;i<(int)adj[u].size();i++){
+          if(adj[u][i]!=p&&(cur_edge!=make_pair(adj[u][i],u))&&(cur_edge!=make_pair(u,adj[u][i]))){
+             dfs(adj[u][i],u,depth+1);
+          }
+       }
 }
-
-
+int solve(int u){
+	
+        mxdp=-1;
+        dfs(u,-1,0);
+        mxdp=-1;
+        dfs(v,-1,0);
+        return mxdp;
+}
 int main(){
-     scanf("%d",&n);
-     for(int i=0,a,b;i<n-1;i++){
-        scanf("%d %d",&a,&b);
-        adj[a][b]=adj[b][a]=1;
-     }
-     int finalans=0;
-     for(int i=1;i<=n;i++){
-         for(int j=1;j<=n;j++){
-              int a1=0,b1=0;
-              if(adj[i][j]){
-                 ans=0;memset(dep,0,sizeof(dep));
-                  a1=dfs(i,j);
-                 ans=0;memset(dep,0,sizeof(dep));
-                  b1=dfs(j,i);
-              }
-               finalans=max(a1*b1,finalans);
-         }
-        
-     }
-     printf("%d\n",finalans);
+       int n;
+       scanf("%d",&n);
+       for(int i=0,a,b;i<n-1;i++){
+         scanf("%d %d",&a,&b);
+         adj[a].push_back(b);
+         adj[b].push_back(a);
+         edges.push_back(make_pair(a,b));
+       }
+       for(int i=0;i<n-1;i++){
+           cur_edge=edges[i];
+           int dep1=solve(edges[i].first);
+           int dep2=solve(edges[i].second);
+           ans=max(ans,dep1*dep2);
+       }
+       printf("%d",ans);
 }
