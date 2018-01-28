1acb2f74dedd7297bbd32c634c1096d3d708eba8
Michael Mounir <mikaelmounir@gmail.com>
2018-01-28 09:02:08 +0200

    Solve UVA_11515

--- UVA/UVA_11515.cpp
+++ UVA/UVA_11515.cpp
@@ -1,8 +1,8 @@
 /**
 *   @Author : MeGaCrazy
 *   @InitTime : Sat Jan 13 20:14:28 2018
-*   @Idea :
-*
+*   @Idea :  Just Make KnapSack With Checking if The the two circle intersect 
+*            Only if radius of circle 1 + circle 2 >= the distance between two circle center :)
 *
 *
 *
@@ -18,64 +18,16 @@ const int N = 16;
 crane a[N];
 int dp[N][1 << N];
 int n;
-pair<int, int>point[N];
-pair<int, int>curpoint[N];
-int xx[] = { -1,-1,1,1 };
-int yy[] = { -1,1,1,-1 };
-double in(double s,double e,double x){
- return x >=s && x <= e;
+int dis(int x,int y,int xx,int yy){
+   return (x-xx)*(x-xx)+(y-yy)*(y-yy);
 }
-bool PointOn(seg &S,point & P){
-    double minx,maxx,miny,maxy;
-    minx=min(S.s.x,S.e.x);
-    maxx=max(S.s.x,S.e.x);
-    miny=min(S.s.y,S.e.y);
-    maxy=max(S.s.y,S.e.y);
-    if(in(minx,maxx,P.x) !=0  && in(miny,maxy,P.y)!=0){
-       return ((S.s.x - S.e.x)*(P.y - S.s.y) == (S.s.y - S.e.y) * (P.x - S.s.x));
-    }
-     return 0;
-}
-int cross(pair<int, int> o, pair<int, int> a, pair<int, int> b) {
-	return (int)(a.first - o.first)*(b.second - o.second) - (a.second - o.second)*(b.first - o.first);
-}
-
-bool check(int i, int j) {
-       if(PointOn(a[i],a[j].s)||PointOn(a[i],a[j].e)||PointOn(a[j],a[i].s)||PointOn(a[j],a[i].e)){
-             return 1;
-         }
-	if (cross(point[i], point[i + 1], curpoint[j])*cross(point[i], point[i + 1], curpoint[j + 1])<0 &&
-		cross(point[i + 1], point[i], curpoint[j])*cross(point[i + 1], point[i], curpoint[j + 1])<0 &&
-		cross(curpoint[j], curpoint[j + 1], point[i])*cross(curpoint[j], curpoint[j + 1], point[i + 1])<0 &&
-		cross(curpoint[j + 1], curpoint[j], point[i])*cross(curpoint[j + 1], curpoint[j], point[i + 1])<0
-		) return 1;
-	return 0;
-}
-int valid(int idx) {
-	int len = 0;
-	for (int j = 0; j<4; j += 2) {
-		int L = a[idx].l;
-		curpoint[len++] = make_pair(a[idx].x + L*xx[j], a[idx].y + L*yy[j]);
-	}
-	for (int j = 1; j<4; j += 2) {
-		int L = a[idx].l;
-		curpoint[len++] = make_pair(a[idx].x + L*xx[j], a[idx].y + L*yy[j]);
-	}
-	for (int i = 0; i<n; i++) {
-		if (i == idx)continue;
-		int L = a[i].l;
-		int X = a[i].x;
-		int Y = a[i].y;
-		for (int j = 0; j<4; j++) {
-			point[j] = make_pair(X + L*xx[j], Y + L*yy[j]);
-		}
-	}
-	for (int i = 0; i<4; i += 2) {
-		for (int j = 0; j<4; j += 2) {
-			if (check(i, j))return 0;
-		}
-	}
-	return 1;
+bool valid(int idx,int mask){
+      for(int i=0;i<n;i++){
+         if(idx==i||(mask & (1<<i))==0)continue;
+         int d=dis(a[i].x,a[i].y,a[idx].x,a[idx].y);
+      	 if((a[i].l+a[idx].l)*(a[i].l+a[idx].l) >= d)return 0;
+      }
+      return 1;
 }
 int solve(int idx, int mask) {
 	if (idx==n)
@@ -84,7 +36,7 @@ int solve(int idx, int mask) {
 	if (ret != -1)
 		return  ret;
 	int ch1 = 0;
-	if ((mask & (1 << idx)) == 0 && valid(idx))ch1 = solve(idx + 1, mask | (1 << idx)) + a[idx].l * a[idx].l;
+	if ((mask & (1 << idx)) == 0 && valid(idx,mask))ch1 = solve(idx + 1, mask | (1 << idx)) + a[idx].l * a[idx].l;
 	ret = max(ch1, solve(idx + 1, mask));
 	return ret;
 }
@@ -101,8 +53,6 @@ int main() {
 		scanf("%d %d %d", &a[i].x, &a[i].y, &a[i].l);
 	}
 	memset(dp, -1, sizeof(dp));
-	double pi = acos(-1);
-//	int f=(int)((double)solve(0,0) / pi);
 	printf("%d\n", solve(0,0));
 	}
 }
